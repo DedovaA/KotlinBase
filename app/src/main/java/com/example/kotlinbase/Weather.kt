272484b921f7aa81f3aa70a6cd59f55aa077db58
asya <asyatim@mail.ru>
2022-03-28 22:48:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/kotlinbase/Weather.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinbase
+
+data class Weather(var city: String, var temperature: Int)
+
+
+//class Repository {
+//    private val weatherList: List<Weather> = listOf(Weather("Moscow", 15))
+//}
