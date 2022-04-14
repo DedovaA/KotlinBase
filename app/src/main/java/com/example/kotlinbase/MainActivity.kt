8138b68296d0fd7e4145f380764b82db352ef122
asya <asyatim@mail.ru>
2022-04-15 00:35:06 +0600

    lesson2 started

--- app/src/main/java/com/example/kotlinbase/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbase/MainActivity.kt
@@ -2,21 +2,17 @@ package com.example.kotlinbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import com.example.kotlinbase.databinding.ActivityMainBinding
-import java.util.*
+import com.example.kotlinbase.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
--- app/src/main/java/com/example/kotlinbase/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kotlinbase
-
-data class User(val name: String = "", val age: Int = 11)
