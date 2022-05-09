3e760d221c28f05e127032b4bd82da70bd7db0d7
asya <asyatim@mail.ru>
2022-05-10 01:35:45 +0600

    HW lesson5. Added apikey.properties.

--- app/src/main/java/com/example/kotlinbase/repository/WeatherLoader.kt
+++ app/src/main/java/com/example/kotlinbase/repository/WeatherLoader.kt
@@ -2,7 +2,7 @@ package com.example.kotlinbase.repository
 
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
+import com.example.kotlinbase.BuildConfig
 import com.google.gson.Gson
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -25,7 +25,7 @@ class WeatherLoader (private val onServerResponseListener: OnServerResponse) {
                         requestMethod = "GET"
                         connectTimeout = 1000
                         readTimeout = 1000
-                        addRequestProperty("X-Yandex-API-Key", "")
+                        addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
                     }
 
                     val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
